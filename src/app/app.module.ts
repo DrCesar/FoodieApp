ef932e6c1271e532b45df17e2ffb7cc33c5cd916
Unknown <josuejacobstercero@gmail.com>
2017-10-28 12:12:13 -0600

    Se adapta para simular en Android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
-import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
-import { File } from '@ionic-native/file';
+import { FileOpener } from '@ionic-native/file-opener';
+import { FileChooser } from '@ionic-native/file-chooser';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -34,10 +34,8 @@ import { UserProvider } from '../providers/user.provider';
     SplashScreen,
     UserProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    FileTransfer,
-    FileUploadOptions,
-    FileTransferObject,
-    File
+    FileOpener,
+    FileChooser
   ]
 })
 export class AppModule {}
--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { FileChooser } from '@ionic-native/file-chooser';
+import { FileOpener } from '@ionic-native/file-opener';
+import { File } from '@ionic-native/file';
 /**
  * Generated class for the UploadPage page.
  *
@@ -10,16 +12,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-upload',
-  templateUrl: 'upload.html',
+	selector: 'page-upload',
+	templateUrl: 'upload.html',
 })
 export class UploadPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	imgPath;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UploadPage');
-  }
+	constructor(private fileChooser: FileChooser, public navCtrl: NavController, public navParams: NavParams) {
+		this.imgPath = '../../assets/img/empty.png';
+	}
 
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad UploadPage');
+	}
+
+	uploadImg() {
+		this.fileChooser.open()
+			.then(uri => console.log(uri))
+			.catch(e => console.log(e));
+	}
 }
