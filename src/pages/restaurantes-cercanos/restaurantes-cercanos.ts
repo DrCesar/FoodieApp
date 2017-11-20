df211047dcd17df6615fe54b521c328619a10b15
Barrios596 <barrios_596@hotmail.com>
2017-11-20 14:04:25 -0600

    Add files via upload

--- src/pages/restaurantes-cercanos/restaurantes-cercanos.ts
+++ src/pages/restaurantes-cercanos/restaurantes-cercanos.ts
@@ -1,28 +1,13 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-declare var google: any;
+
 @Component({
   selector: 'page-restaurantes-cercanos',
   templateUrl: 'restaurantes-cercanos.html'
 })
 export class RestaurantesCercanosPage {
 
-  @ViewChild('map') mapRef: ElementRef;
-  map: any;
   constructor(public navCtrl: NavController) {
-  }
-
-  ionViewDidLoad(){
-    this.showMap();
-  }
-  showMap(){
-    const location = new google.maps.LatLng(14.604610,-90.488846);
-    const options ={
-      center: location,
-      zoom: 16 
-    }
-    
-    this.map = new google.maps.Map(this.mapRef.nativeElement,options);
   }
   
 }
